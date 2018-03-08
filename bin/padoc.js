30c2791c71285d20538af64251da1f6f132dd98c
AHN HO JUNG <labeldock@me.com>
2018-03-09 02:07:26 +0900

    update

--- bin/padoc.js
+++ bin/padoc.js
@@ -1,6 +1,46 @@
 #!/usr/bin/env node
+const path = require("path")
+const argv = {
+  signal:"compile",
+  from:"es6",
+  args:[]
+}
 
-console.log("padoc command!");
-process.argv.forEach((val, index) => {
-  console.log(`${index}: ${val}`);
+let castCommand = null;
+process.argv.forEach((value, index) => {
+  if(index == 1){
+    argv.root = path.resolve(value,"../../../") 
+  }
+  
+  if(index == 2){
+    argv.src = path.resolve(argv.root,value)
+  }
+  
+  if(index > 2){
+    if(!castCommand && value.indexOf("-") === 0){
+      switch(value){
+        case "-s": case "--signal":
+          castCommand = "signal"
+          break
+        case "-o": case "--out":
+          castCommand = "out"
+          break
+        case "-m": case "--module":
+          castCommand = "module"
+          break
+        default
+          console.warn(`unknown command ${value}`)
+          break
+      }
+    } else {
+      if(castCommand){
+        argv[castCommand] = value
+        castCommand       = null
+      } else {
+        argv.args.push(castCommand)
+      }
+    }
+  }
 });
+
+console.log("padoc command!\n",argv)
